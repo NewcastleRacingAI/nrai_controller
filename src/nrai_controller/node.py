1c4bc6af2e85220e89bf341ce3018c4cc5b57748
GRP-Projects <gabriel.reece.perry@outlook.com>
2026-06-30 20:02:49 +0100

    Modified node to be consistent with new schema

---
+++ src/nrai_controller/node.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+import struct
+import os
+import math
+import time
+import pickle
+
+from purepursuit import get_angle
+
+fifo_out = '/tmp/lower_ctrl_cmd'
+
+class AckermannDrive():
+    def __init__(self):
+        self.steering_angle = float(0)
+        self.steering_angle_velocity = float(1)
+        self.speed = float(0.5)
+        self.acceleration = float(0.25)
+        self.jerk = float(0.1)
+
+def main(args: argparse.Namespace):
+
+    topics: dict[str, Queue] = args.topics or {}
+
+    # --- Set up Code ---
+    if args.control_topic not in topics:
+        raise ValueError(f"No '{args.control_topic}' topic to listen to.")
+
+    control_queue = topics[args.control_topic]
+
+    while True:
+        path = control_queue.get()
+        drive = AckermannDrive()
+
+        get_angle(path, drive)
+        steering_angle_velocity = float(1)
+        speed = float(0.5)
+        acceleration = float(0.25)
+        jerk = float(0.1)
+
+        new_instruction = struct.pack('<5fI', drive.steering_angle, drive.steering_angle_velocity, drive.speed, drive.acceleration, drive.jerk, 0xFFFFFFFF)
+        try:
+            fd = os.open(fifo_out, os.O_WRONLY)
+            with open(fd, "wb") as fifo:
+                fifo.write(new_instruction)
+        except FileNotFoundError:
+            print(f"NRAI_CONTROLLER: Could not access FIFO {fifo_out}. Likely not yet configured.")
+        except BrokenPipeError:
+            print(f"NRAI_CONTROLLER: FIFO {fifo_out} terminated.")
+
+if __name__ == '__main__':
+    main()
